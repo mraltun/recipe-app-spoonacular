8a6a5841c878e8d11f0915178ecca577ff2f739a
Murat Altun <mraltun@gmail.com>
2022-10-22 15:32:47 +0300

    Get the popular recipes and render them inside Popular. Also installed some dependencies

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,19 +1,50 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
 
 const Popular = () => {
+  const [popular, setPopular] = useState([]);
+
   useEffect(() => {
     getPopular();
   }, []);
 
   const getPopular = async () => {
     const api = await fetch(
-      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
+      `https://apis.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
     );
     const data = await api.json();
-    console.log(data);
+    setPopular(data.recipes);
   };
 
-  return <div>Popular</div>;
+  return (
+    <Wrapper>
+      <h3>Popular pics</h3>
+      {popular.map((recipe) => {
+        return (
+          <Card key={recipe.id}>
+            <p>{recipe.title}</p>
+            <img src={recipe.image} alt={recipe.title} />
+          </Card>
+        );
+      })}
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  margin: 4rem 0rem;
+`;
+
+const Card = styled.div`
+  min-height: 25rem;
+  border-radius: 2rem;
+  overflow: hidden;
+
+  img {
+    border-radius: 2rem;
+  }
+`;
 
 export default Popular;
