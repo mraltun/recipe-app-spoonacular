d9790cffcc876184e06b9ac0df2f2eded7b428d2
Murat Altun <mraltun@gmail.com>
2022-10-22 20:22:32 +0300

    Added Search and input field with it's css

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import Pages from "./pages/Pages";
 import Category from "./components/Category";
+import Search from "./components/Search";
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
+        <Search />
         <Category />
         <Pages />
       </BrowserRouter>
