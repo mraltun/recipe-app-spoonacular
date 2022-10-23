e5a7afc4116c8e85e23ae46b016d95e98f757421
Murat Altun <mraltun@gmail.com>
2022-10-23 13:55:00 +0300

    Added Nav and Logo inside App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Link } from "react-router-dom";
+import styled from "styled-components";
 import Pages from "./pages/Pages";
 import Category from "./components/Category";
 import Search from "./components/Search";
+import { GiKnifeFork } from "react-icons/gi";
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
+        <Nav>
+          <GiKnifeFork />
+          <Logo to='/'>Recipes</Logo>
+        </Nav>
         <Search />
         <Category />
         <Pages />
@@ -15,5 +21,23 @@ const App = () => {
     </div>
   );
 };
+
+const Logo = styled(Link)`
+  text-decoration: none;
+  font-size: 1.5rem;
+  font-weight: 400;
+  font-family: "Lobster Two", cursive;
+`;
+
+const Nav = styled.div`
+  padding: 4rem 0rem;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+
+  svg {
+    font-size: 2rem;
+  }
+`;
 
 export default App;
