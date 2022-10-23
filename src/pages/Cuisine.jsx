871d9edf7e816234b5b7c29106f6c0dc611f82e0
Murat Altun <mraltun@gmail.com>
2022-10-23 15:29:00 +0300

    Added Framer Motion

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -20,7 +20,12 @@ const Cuisine = () => {
   }, [params.type]);
 
   return (
-    <Grid>
+    <Grid
+      animate={{ opacity: 1 }}
+      initial={{ opacity: 0 }}
+      exit={{}}
+      transition={{ duration: 0.5 }}
+    >
       {cuisine.map((item) => {
         return (
           <Card key={item.id}>
@@ -35,7 +40,7 @@ const Cuisine = () => {
   );
 };
 
-const Grid = styled.div`
+const Grid = styled(motion.div)`
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
   grid-gap: 3rem;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,19 @@
 import React from "react";
+import { motion } from "framer-motion";
 import Veggie from "../components/Veggie";
 import Popular from "../components/Popular";
 
 const Home = () => {
   return (
-    <div>
+    <motion.div
+      animate={{ opacity: 1 }}
+      initial={{ opacity: 0 }}
+      exit={{}}
+      transition={{ duration: 0.5 }}
+    >
       <Veggie />
       <Popular />
-    </div>
+    </motion.div>
   );
 };
 
