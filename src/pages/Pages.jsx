82c8549de892452025a8b6fa4f24fda4458310c4
Murat Altun <mraltun@gmail.com>
2022-10-23 11:53:01 +0300

    Added Searched and it's route in Pages

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Cuisine from "./Cuisine";
+import Searched from "./Searched";
 
 const Pages = () => {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='cuisine/:type' element={<Cuisine />} />
+      <Route path='searched/:search' element={<Searched />} />
     </Routes>
   );
 };
